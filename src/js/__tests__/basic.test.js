ea6ef2f06667a5a597352d337eae3f8212e364ca
tolik19bat <play.hoffa@gmail.com>
2023-12-11 07:02:41 +0300

    first_working_code_commit

--- src/js/__tests__/basic.test.js
+++ src/js/__tests__/basic.test.js
@@ -5,3 +5,42 @@ test('should sum', () => {
 
   expect(result).toBe(6);
 });
+//========================================================
+
+import { Magician, Daemon } from '../app'; //путь к нашим классам
+
+
+    test('getAttack должен правильно рассчитывать атаку, не будучи забит камнями', () => {
+      const magician = new Magician(100, 2);
+      expect(magician.getAttack()).toBe(90);
+    });
+
+    test('getAttack должен правильно рассчитывать атаку с камнями', () => {
+      const magician = new Magician(100, 2);
+      magician.isStoned = true;
+      expect(magician.getAttack()).toBe(85);
+    });
+
+    test('setDistance следует обновить расстояние', () => {
+      const magician = new Magician(100, 2);
+      magician.setDistance(3);
+      expect(magician.distance).toBe(3);
+    });
+
+
+    test('getAttack должен правильно рассчитывать атаку не будучи забит камнями', () => {
+      const daemon = new Daemon(120, 5);
+      expect(daemon.getAttack()).toBe(72);
+    });
+
+    test('getAttack должен правильно рассчитывать атаку с камням', () => {
+      const daemon = new Daemon(120, 5);
+      daemon.isStoned = true;
+      expect(daemon.getAttack()).toBe(54);
+    });
+
+    test('setDistance следует обновить состояние', () => {
+      const daemon = new Daemon(120, 5);
+      daemon.setDistance(4);
+      expect(daemon.distance).toBe(4);
+    });
--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,102 @@
 // TODO: write your code here
-import sum from './basic';
+// import sum from './basic';
+//
+// console.log('worked');
+//
+// console.log(sum([1, 2]));
+//=====================================================
 
-console.log('worked');
+"use strict"
+// export default
+class Math {//создаём класс от которого будут наследоваться
+  constructor(attack, distance) {//
+    this.attack = attack;
+    this.distance = distance;
+  }
 
-console.log(sum([1, 2]));
+  getAttackWithDistanceModifier() {//модификатор для коррекции линейного значения
+    const distanceModifier = 1 - 0.1 * (this.distance - 1);
+    return this.attack * distanceModifier;
+  }
+
+  setDistance(distance) {
+    this.distance = distance;
+  }
+}
+
+class Magician extends Math {
+  constructor(attack, distance, stoned = false) {
+    super(attack, distance);
+    this.stoned = stoned;
+  }
+
+  getAttack() {
+    // function mathLog(x) {
+    //   return Math.log(x) / Math.log(2);
+    // }
+
+function mathLog2() {
+Math.log2 ||
+function (x) {
+  return Math.log(x) / Math.LN2;
+}}
+
+    // const math = mathLog(this.distance)
+    const attackWithDistanceModifier = this.getAttackWithDistanceModifier();
+    if (this.stoned) {
+      const stonedAttack = attackWithDistanceModifier - (mathLog2(this.distance) * 5);
+      return stonedAttack >= 0 ? stonedAttack : 0;
+    }
+    return attackWithDistanceModifier;
+  }
+
+  get isStoned() {
+    return this.stoned;
+  }
+
+  set isStoned(value) {
+    this.stoned = value;
+  }
+}
+
+class Daemon extends Math {
+  constructor(attack, distance, stoned = false) {
+    super(attack, distance);
+    this.stoned = stoned;
+  }
+
+  getAttack() {
+    function mathLog2() {
+      Math.log2 ||
+      function (x) {
+        return Math.log(x) / Math.LN2;
+      }}
+    const attackWithDistanceModifier = this.getAttackWithDistanceModifier();
+    if (this.stoned) {
+      const stonedAttack = attackWithDistanceModifier - ((mathLog2(this.distance)) * 5);
+      return stonedAttack >= 0 ? stonedAttack : 0;
+    }
+    return attackWithDistanceModifier;
+  }
+
+  get isStoned() {
+    return this.stoned;
+  }
+
+  set isStoned(value) {
+    this.stoned = value;
+  }
+}
+
+// Пример использования
+const magician = new Magician(100, 2);
+console.log(magician.getAttack()); // Выведет 90
+
+const daemon = new Daemon(120, 5);
+console.log(daemon.getAttack()); // Выведет 72
+
+magician.isStoned = true;
+daemon.isStoned = true;
+
+console.log(magician.getAttack()); // Выведет 85
+console.log(daemon.getAttack()); // Выведет 54
